6bde8e8d1cb3e97b956ed8cfac8bbe92dbce5afc
alla <alla086@rambler.ru>
2026-04-14 22:53:06 +0300

    Добавила первый файл

---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,17 @@
+from smartphone import Smartphone
+
+# Объявляем переменную catalog и наполняем её экземплярами Smartphone
+
+catalog = [
+
+    Smartphone("Apple", "iPhone 15", "+79211234567"),
+
+    Smartphone("Samsung", "Galaxy S24", "+79212345678"),
+
+    Smartphone("Xiaomi", "Redmi Note 13", "+79213456789"),
+
+    Smartphone("Google", "Pixel 8", "+79214567890"),
+
+    Smartphone("HONOR", "Magic 6", "+79215678901")
+
+]
