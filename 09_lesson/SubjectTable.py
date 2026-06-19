a53a2ceeb4acfdd3569b649ff0efa0e9e09f9357
alla <alla086@rambler.ru>
2026-06-19 18:37:01 +0300

    9 ДЗ

--- 09_lesson/SubjectTable.py
+++ 09_lesson/SubjectTable.py
@@ -1,14 +1,10 @@
 from sqlalchemy import create_engine, inspect, text
-from sqlalchemy import create_engine,  text
 
 
 class SubjectTable:
     __scripts = {
         'select':text("SELECT * FROM subject"),
-        'insert_new':text("INSERT INTO subject (subject_id, subject_title) VALUES(:new_id, :new_subject)"),
-        'update_subject':text("UPDATE subject SET subject_id=:new_sub_id WHERE subject_title=:new_subject_title"),
-        'delete_by_subject_title':text("DELETE FROM subject WHERE subject_title=:subject_to_delete")
-        'select': text("SELECT * FROM subject"),
+
         'insert_new': text(
             "INSERT INTO subject (subject_id, subject_title)"
             " VALUES(:new_id, :new_subject)"
@@ -22,9 +18,7 @@ class SubjectTable:
             "WHERE subject_title=:subject_to_delete")
         }
 
-    def __init__(self, connection_string):
-        conn.close()
-        return rows
+    self.__db = create_engine(connection_string)
 
     def create_subject(self,new_id, subject_name):
     def create_subject(self, new_id, subject_name):
@@ -32,20 +26,18 @@ class SubjectTable:
         conn.execute(self.__scripts['insert_new'], {'new_id': new_id, 'new_subject': subject_name})
         conn.execute(self.__scripts['insert_new'],
                      {'new_id':new_id, 'new_subject':subject_name})
-        conn.commit()
+
         conn.close()
 
     def update_subject(self, new_id, title):
         conn = self.__db.connect()
         conn.execute(self.__scripts['update_subject'], {'new_sub_id': new_id, 'new_subject_title': title})
-        conn.execute(self.__scripts['update_subject'],
                      {'new_sub_id':new_id, 'new_subject_title':title})
         conn.commit()
         conn.close()
 
     def delete_subject(self, subject_name):
         conn = self.__db.connect()
-        conn.execute(self.__scripts['delete_by_subject_title'], {'subject_to_delete': subject_name})
         conn.execute(self.__scripts['delete_by_subject_title'],
                      {'subject_to_delete':subject_name})
         conn.commit()
